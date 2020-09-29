c14f54e4436cd845ec15f2546a929c491b9083a0
MonsterB3 <71831256+MonsterB3@users.noreply.github.com>
2020-09-29 13:33:34 +0200

    Delete classe Carte.py

--- classe Carte.py
+++
@@ -1,8 +0,0 @@
-class Carte():
-    def __init__(self,couleur,rang):
-        RANGS =["2","3","4","5","6","7","8","9","X","V","D","R","A"]
-        COULEURS = ["P","C","K","T"]
-        self.nbcouleur=couleur
-        self.couleur=COULEURS[self.nbcouleur]
-        self.nbrang=rang
-        self.rang=RANGS[self.nbrang]
