7814f591573a596345b6acf60e8b0e215fd275dc
Gelonch <71832792+Gelonch@users.noreply.github.com>
2020-10-01 16:52:13 +0200

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ class Croupier(Carte):
 			paquet -> paquet de 52 carte
 	"""
 	def __init__(self):
-		Carte.__init__()
+		Carte.__init__(self)
 		self.paquet = []
 	def rassembler(self): #Méthode qui crée 52 cartes non mélangé
 		for i in range(0, len(self.COULEURS)):
@@ -28,7 +28,7 @@ class Croupier(Carte):
 		rand = randint(1, 50)
 		t_paquet = []
 		for i in range (0, rand):
-			t_paquet.append(self.paquet[0])
+			_paquett.append(self.paquet[0])
 			self.paquet.pop(0)
 		for i in range (0, len(t_paquet)):
 			self.paquet.append(t_paquet[0])
@@ -42,20 +42,20 @@ class Croupier(Carte):
 
 
 class Joueur(Croupier):
-    def __init__(self , nom, main, tapis=500):
+
+    def __init__(self , nom = "Joueur", main = None, tapis=500):
         Croupier.__init__(self)
         self.nom=nom
         self.main=main#les cartes en main sont une liste
         self.tapis=tapis#tapis étant le nombre de jetons
 
-        def main(self):#affiche la main , à enlevée quand ce sera pris en charge par le programme principal
-            return (self.main)
+    def vider_main(self):#on supprime la main actuelle
+        for i in range(0,5):#le mode de jeu est de 5 carte privatives...
+            self.main.pop()#on les supprimes
 
-        def nouvelle_donne(self):#on supprime la main actuelle
-            for i in range(0,5):#le mode de jeu est de 5 carte privatives...
-                self.main.pop()#on les supprimes
-            
-        def recevoir(self,paquet):
-            for i in range(5):
-                self.main.append(self.paquet[-1])
-                self.paquet.pop()#besoin de 5 nouvelles cartes, en fonction de croupier.. en attente de plus d'info sur la classe
+    def recevoir(self):
+    	if len(self.main) != 0 :
+    		vider_main()
+        for i in range(5):
+        	self.main.append(self.paquet[-1])
+        	self.paquet.pop()#besoin de 5 nouvelles cartes, en fonction de croupier.. en attente de plus d'info sur la classe
